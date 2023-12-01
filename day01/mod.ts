53c78d3150c341885002d857153a682537c65c13
Maik <lxcalflxw@gmail.com>
2023-12-01 20:50:19 +0100

    feat: finishes day 01

--- day01/mod.ts
+++ day01/mod.ts
@@ -23,42 +23,29 @@ export function day01_02() {
     'eight': 8,
     'nine': 9
   }
+
   const lines = readFileLines('day01/input.txt');
 
   let totalSum = 0;
-  for (let line of lines) {
-    let str = "";
-    console.log("Line before: " + line);
-    
-    line
-      .split('')
-      .filter(char => isNaN(+char))
-      .forEach(char => {/* 
-        console.log('---')
-        console.log("str: " + str);
-        console.log("c: " + char);
-        console.log("Entries: " + Object.keys(digits).some(entry => entry.startsWith(str))); */
-        
-        if (!Object.keys(digits).some(entry => entry.startsWith(str + char))) {
-          str = "";
-        }
-        str += char;
-        if (Object.keys(digits).find(entry => entry === str)) {
-          line = line.replace(
-            str,
-            digits[Object.keys(digits).find((entry) => entry === str) as keyof typeof digits].toString(),
-          );
-          str = "";
-        }
-      });
+  for (const line of lines) {
+    const numbers: number[] = []; 
 
-    console.log('Line after: ' + line);
-    const numbers: number[] = line
-      .split('')
-      .filter((char) => !isNaN(+char))
-      .map((digit) => +digit);
+    for (let i = 0; i < line.length; i++) {
+      const char = line.split('')[i];
+      if (!isNaN(+char)) {
+        numbers.push(+char)
+        continue;
+      }
+
+      for (const key of Object.keys(digits)) {
+        if (line.slice(i).startsWith(key)) {
+          numbers.push(+digits[key as keyof typeof digits]);
+        }
+      }
+    }
+    
     totalSum += numbers[0] * 10 + numbers[numbers.length - 1];
   }
 
-  console.log("Total Sum: " + totalSum)
+  console.log("Total Sum: " + totalSum);
 }
